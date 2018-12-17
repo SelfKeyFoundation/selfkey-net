fbda22488d971fc76b217feb113c6f7b5aa3b1af
Jason Payne <github@x-treem.net>
2018-12-17 03:37:38 +0100

    Update packages for publication to NuGet.

--- SelfKey.Login.Data/Models/Claim.Entity.cs
+++ SelfKey.Login.Data/Models/Claim.Entity.cs
@@ -1,8 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
-#pragma warning disable IDE1006 // Naming Styles
-
 namespace SelfKey.Login.Data.Models
 {
     public partial class Claim
